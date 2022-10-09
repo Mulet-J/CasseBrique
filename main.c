174a6b8bd0d1b6c7ca4c250cd2e32074f601fae8
Mulet-J <jules.mulet@gmx.fr>
2022-10-10 00:46:39 +0200

    changements type, non fini

--- main.c
+++ main.c
@@ -14,49 +14,18 @@ typedef struct Bomb{
     int strength;
 } Bomb;
 
-typedef struct  Case {
+typedef struct  Tile {
     int wall;
     int powerUP;
     Player player;
     Bomb bomb;
-} Case;
+} Tile;
 
 typedef struct Map {
     int width;
     int height;
-    int **grid;
+    Tile **tileGrid;
 } Map;
-
-/*
- * 0 = vide
- * 1-4 = joueurs
- * 5 = mur indestructible
- * 6 = mur destructible
- */
-Map createMap(int height, int width){
-    int player = 1;
-    Map myMap = {
-            .width = width,
-            .height = height,
-            .grid = malloc(sizeof(int)*(height*2)),  //marche pas avec seulement height
-    };
-    for (int i = 0; i < myMap.height; ++i) {
-        myMap.grid[i] = malloc(sizeof(int)*myMap.width);
-        for (int j = 0; j < myMap.width; ++j) {
-            if (i == 0 || j == 0 || i == height - 1 || j == width - 1) {
-                myMap.grid[i][j] = 5;
-            } else if ((i==1 || i == height-2) && (j==1 || j==width-2)) {
-                myMap.grid[i][j] = player;
-                player++;
-            } else if (i%2 == 0 && j%2 == 0) {
-                myMap.grid[i][j] = 6;
-            } else {
-                myMap.grid[i][j] = 0;
-            }
-        }
-    }
-    return myMap;
-}
 
 void movePlayer(Map *myMap,int playerID, char direction){
     switch (direction) {
@@ -77,35 +46,74 @@ void bombExplode(Map *myMap,int x, int y){
 
 }
 
+Player newPlayer(int playerID){
+    Player myPlayer = {
+            .bombCount = 1,
+            .bombStrength = 2,
+            .health = 1,
+            .invincibility = 0,
+            .playerID = playerID,
+    };
+    return myPlayer;
+}
+
+Bomb newBomb(int strength){
+    Bomb myBomb = {
+            .strength = strength,
+    }
+}
+
+Map newMap(int height, int width) {
+    //reste initialiser toutes les valeurs même vides
+    int playerID = 1;
+    Map myMap = {
+            .height = height,
+            .width = width,
+            .tileGrid = malloc(sizeof(Tile)*height),
+    };
+    for (int x = 0; x < myMap.height; ++x) {
+        myMap.tileGrid[x] = malloc(sizeof(Tile)*height);
+        for (int y = 0; y < myMap.width; ++y) {
+            myMap.tileGrid[x][y].wall = 0;
+            myMap.tileGrid[x][y].powerUP = 0;
+            myMap.tileGrid[x][y].player. = ;
+            if (x == 0 || y == 0 || x == height - 1 || y == width - 1) {
+                myMap.tileGrid[x][y].wall = 1;
+            } else if ((x==1 || x == height-2) && (y==1 || y==width-2)) {
+                Player myPlayer = newPlayer(playerID);
+                myMap.tileGrid[x][y].player = myPlayer;
+                playerID++;
+                //player
+            } else if (x%2 == 0 && y%2 == 0) {
+                //mur milieu carte
+            } else {
+                //vide
+            }
+        }
+    }
+    return myMap;
+}
+
 void printMap(Map *myMap){
     system("cls");  //vide la console sous windows
-    for (int i = 0; i < myMap->height; ++i) {
-        for (int j = 0; j <myMap->width; ++j) {
-            switch (myMap->grid[i][j]) {
-                case 0:
-                    printf("  ");
-                    break;
-                case 1:
-                case 2:
-                case 3:
-                case 4:
-                    printf("%d ", myMap->grid[i][j]);
-                    break;
-                case 5:
-                    printf("x ");
-                    break;
-                case 6:
-                    printf("# ");
-                    break;
+    for (int x = 0; x < myMap->height; ++x) {
+        for (int y = 0; y <myMap->width; ++y) {
+            if(myMap->tileGrid[x][y].wall==1){
+                printf("X ");
+            } else if(myMap->tileGrid[x][y].wall==2) {
+                printf("# ");
+            } else if(myMap->tileGrid[x][y].player) {
+
+            } else {
+                printf("  ");
             }
-            //printf("%d ",myMap->grid[i][j]);
         }
         printf("\n");
     }
 }
 
 int main() {
-    Map myMap = createMap(11,11);
+    Map myMap = newMap(11,6);
     printMap(&myMap);
     /*
      * boucle de jeu :
@@ -116,6 +124,5 @@ int main() {
      * check joueur en vie
      */
 
-    //printf("Hello, World!\n");
     return 0;
 }
